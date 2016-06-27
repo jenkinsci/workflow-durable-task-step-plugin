65f8b6f9e2a200ed4f68b10eae3a28a2489e1195
Jesse Glick <jglick@cloudbees.com>
2016-06-27 15:23:55 -0400

    [JENKINS-26130] [JENKINS-31842] Provide printed form for pickle futures.

--- src/main/java/org/jenkinsci/plugins/workflow/support/pickles/FilePathPickle.java
+++ src/main/java/org/jenkinsci/plugins/workflow/support/pickles/FilePathPickle.java
@@ -51,6 +51,9 @@ public class FilePathPickle extends Pickle {
             protected FilePath tryResolve() {
                 return FilePathUtils.find(slave, path);
             }
+            @Override public String toString() {
+                return "Looking for path named ‘" + path + "’ on computer named ‘" + slave + "’";
+            }
         };
     }
 
