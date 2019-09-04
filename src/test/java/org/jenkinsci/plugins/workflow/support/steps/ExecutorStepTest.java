c01bba6d8324b9c23b5a5992e3cfb3baa3cc48d7
Devin Nusbaum <dwnusbaum@users.noreply.github.com>
2019-09-04 09:34:41 -0400

    Fix typo in test
    Co-Authored-By: Jesse Glick <jglick@cloudbees.com>

--- src/test/java/org/jenkinsci/plugins/workflow/support/steps/ExecutorStepTest.java
+++ src/test/java/org/jenkinsci/plugins/workflow/support/steps/ExecutorStepTest.java
@@ -1171,7 +1171,7 @@ public class ExecutorStepTest {
                     "  def isUnix = isUnix()\n" + // Only call `isUnix()` before the agent goes offline to avoid additional log warnings.
                     "  isUnix ? sh('echo hello') : bat('echo hello')\n" +
                     "  semaphore('wait')\n" +
-                    "  isUnix ? sh('echo world') : bat('echo hello')\n" +
+                    "  isUnix ? sh('echo world') : bat('echo world')\n" +
                     "}", true));
             WorkflowRun b = p.scheduleBuild2(0).waitForStart();
             SemaphoreStep.waitForStart("wait/1", b);
