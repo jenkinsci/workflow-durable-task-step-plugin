3bf66fd30ad2a53fca920c4d5264df3091a21d28
Basil Crow <me@basilcrow.com>
2020-08-14 18:14:13 -0700

    Replace deprecated org.junit.Assert.assertThat with non-deprecated org.hamcrest.MatcherAssert.assertThat

--- src/test/java/org/jenkinsci/plugins/workflow/steps/durable_task/ShellStepTest.java
+++ src/test/java/org/jenkinsci/plugins/workflow/steps/durable_task/ShellStepTest.java
@@ -95,7 +95,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 import org.junit.Assume;
 import static org.junit.Assume.assumeFalse;
