c134cc45a40e6582ef62cb9aebb96972390e8f31
Jesse Glick <jglick@cloudbees.com>
2022-05-17 17:51:05 -0400

    Equivalent of #184 for `ExecutorStepDynamicContext`

--- src/main/java/org/jenkinsci/plugins/workflow/support/steps/ExecutorStepDynamicContext.java
+++ src/main/java/org/jenkinsci/plugins/workflow/support/steps/ExecutorStepDynamicContext.java
@@ -29,6 +29,7 @@ import edu.umd.cs.findbugs.annotations.Nullable;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Util;
+import hudson.init.InitMilestone;
 import hudson.model.Computer;
 import hudson.model.Executor;
 import hudson.model.Node;
@@ -87,6 +88,10 @@ public final class ExecutorStepDynamicContext implements Serializable {
     void resume(StepContext context) throws Exception {
         if (executor != null) {
             throw new IllegalStateException("Already resumed");
+        }
+        while (Jenkins.get().getInitLevel() != InitMilestone.COMPLETED) {
+            LOGGER.fine(() -> "waiting to schedule task for " + path + " on " + node + " until Jenkins completes startup");
+            Thread.sleep(100);
         }
         Queue.Item item = Queue.getInstance().schedule2(task, 0).getItem();
         if (item == null) {
