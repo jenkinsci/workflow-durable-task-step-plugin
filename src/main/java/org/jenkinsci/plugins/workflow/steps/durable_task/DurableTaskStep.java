jenkinsci/workflow-durable-task-step-plugin

commit c98bce4c790757658672c109c10f58c592319bd4

PrintStream.close also works. It is a bit scary, however, and produces poorly formatted log text like: missing final newline node=remote watching=true[Pipeline] } which would confuse, for example, pipeline-cloudwatch-logs.

src/main/java/org/jenkinsci/plugins/workflow/steps/durable_task/DurableTaskStep.java
@@ -584,7 +584,6 @@ public abstract class DurableTaskStep extends Step {
             byte[] produce() throws IOException, InterruptedException;
         }
         private void handleExit(int exitCode, OutputSupplier output) throws IOException, InterruptedException {
-            listener().getLogger().println(); // TODO
             Throwable originalCause = causeOfStoppage;
             if ((returnStatus && originalCause == null) || exitCode == 0) {
                 getContext().onSuccess(returnStatus ? exitCode : returnStdout ? new String(output.produce(), StandardCharsets.UTF_8) : null);
@@ -600,6 +599,7 @@ public abstract class DurableTaskStep extends Step {
                     getContext().onFailure(new AbortException("script returned exit code " + exitCode));
                 }
             }
+            listener().getLogger().close();
         }
 
         // ditto
@@ -659,7 +659,6 @@ public abstract class DurableTaskStep extends Step {
                     synchronized (ps) { // like PrintStream.write overloads do
                         IOUtils.copy(stream, os);
                     }
-                    ps.println(); // TODO
                 } else {
                     // A subclass. Who knows why, but trust any write(…) overrides it may have.
                     IOUtils.copy(stream, ps);
@@ -680,7 +679,7 @@ public abstract class DurableTaskStep extends Step {
         }
 
         @Override public void exited(int code, byte[] output) throws Exception {
-            listener.getLogger().flush();
+            listener.getLogger().close();
             execution.exited(code, output);
         }
 

commit 4159ede7f00b87e351fa7e831df3c377ee630b78

c98bce4 broke diagnostic prints inside `handleExit` in `USE_WATCHING` mode since the `NewlineSafeTaskListener` was already closed by `HandlerImpl.exited`

src/main/java/org/jenkinsci/plugins/workflow/steps/durable_task/DurableTaskStep.java
@@ -658,11 +658,11 @@ public abstract class DurableTaskStep extends Step implements EnvVarsFilterableB
                 getContext().onSuccess(returnStatus ? exitCode : returnStdout ? new String(output.produce(), StandardCharsets.UTF_8) : null);
             } else {
                 if (returnStdout) {
-                    listener().getLogger().write(output.produce()); // diagnostic
+                    _listener().getLogger().write(output.produce()); // diagnostic
                 }
                 if (originalCause != null) {
                     // JENKINS-28822: Use the previous cause instead of throwing a new AbortException
-                    listener().getLogger().println("script returned exit code " + exitCode);
+                    _listener().getLogger().println("script returned exit code " + exitCode);
                     getContext().onFailure(originalCause);
                 } else {
                     getContext().onFailure(new AbortException("script returned exit code " + exitCode));